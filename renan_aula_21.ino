de036bdeaa0fb6546265e62c8e74261b9b06ba1a
renan9813 <renan.santi27@gmail.com>
2025-04-17 17:29:24 -0300

    Add files via upload

---
+++ renan_aula_21.ino
@@ -0,0 +1,29 @@
+import processing.serial.*;
+
+Serial myPort;
+printArray(Serial.list());
+
+import processing.serial.*;
+import processing.sound
+
+soundFile menuSong;
+soundFile gameSong;
+
+Serial myPort;
+
+arrayList<integer>(), x = new ArrayList<Interger>(), y  = new Arraylist<Interger>();
+int[] dx = {0, 0, 1, -1}, dy + {1, -1, 0, 0{;
+
+int w = 30, h = 30, bs = 20, dir = 2;
+int applex = 12, appley=10;
+int newdir =0;
+
+int score = 0;
+int state = 0;
+float s = 0;
+
+PImage img1;
+PImage img2;
+
+boolean gameover = false
+string instring;
